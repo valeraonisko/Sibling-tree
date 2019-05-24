3b2bfc47a66023748e2868c9aea8b0a48c040645
Onisko <Onisko@MacBook.local>
2019-05-24 13:24:24 +0300

    all fixed

--- sibling-tree.js
+++ sibling-tree.js
@@ -1,9 +1,8 @@
 const treeUl = document.querySelector('ul');
-const treeLis = document.querySelectorAll('li');
 
-treeUl.addEventListener('click', (event)=>{
+treeUl.addEventListener('click', (event) => {
   const target = event.target;
-  if (target.tagName != 'SPAN') {
+  if (target.tagName !== 'SPAN') {
     return;
   }
   const li = target.parentNode;
